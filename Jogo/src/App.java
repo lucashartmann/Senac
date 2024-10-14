e27edcbdc3ff1d6f3b3d13d569d9d504aa8679ad
Lucas Augusto Hartmann <182410156@senacinformatica.br>
2024-10-14 11:50:18 -0300

    Aula 14.10

---
+++ Jogo/src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class App {
+
+    public void executar(){
+        Jogador cacador = new Jogador(18, Tipo.CAÇADOR);
+        Jogador zumbi = new Jogador(6, Tipo.ZUMBI);
+        Jogador boss = new Jogador(18, Tipo.BOSS);
+
+    }    
+}
