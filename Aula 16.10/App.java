3b342d45cbc4d13bd52dfb8df0dc727b743c69ab
Lucas Augusto Hartmann <78509840+lucashartmann@users.noreply.github.com>
2024-10-16 12:19:44 -0300

    Update

--- Aula 16.10/App.java
+++ Aula 16.10/App.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 public class App {
 
     Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) throws Exception {
         App app = new App();
         //app.algoritmoUm();
         //app.algoritmoDois();
         //app.algoritmoTres();
         //app.algoritmoQuatro();
+        //app.algoritmoCinco();
         app.algoritmoCinco();
     }
 
     public void algoritmoUm() {
         String nome = "Roberto";
         int count = 0;
-
         while (count < 10) {
             System.out.println("Nome: " + nome);
             count = count + 1;
@@ -24,37 +23,28 @@ public class App {
     }
 
     public void algoritmoDois() {
-        int numeroInicial = 1;
-        int numeroFinal = 1000;
-
-        while (numeroInicial <= numeroFinal) {
-            System.out.println(numeroInicial);
-            numeroInicial = numeroInicial + 1;
+        int numero = 1;
+        while (numero <= 1000) {
+            System.out.println(numero);
+            numero = numero + 1;
         }
     }
 
     public void algoritmoTres() {
-        int total = 0;
         int count = 1;
-
         while (count <= 10) {
-            total = count * 9;
-            System.out.println(total);
+            System.out.println(count + " * 9 = " + (count * 9));
             count++;
         }
     }
 
     public void algoritmoQuatro() {
         int numero;
-        int total = 0;
         int count = 1;
-
         System.out.println("Digite um valor");
         numero = sc.nextInt();
-
         while (count <= 10) {
-            total = count * numero;
-            System.out.println(total);
+            System.out.println(count + " * " + numero + " = " + (count * numero));
             count++;
         }
     }
@@ -63,14 +53,14 @@ public class App {
         int n;
         int count = 0;
         int soma = 0;
-
         System.out.println("Digite um valor");
         n = sc.nextInt();
-
-        while (count < n) {
-            soma = count + n;
-            System.out.println("Soma dos números: " + soma);
-            count++;
+        while (count <= n) {
+            System.out.println(soma + " + " + count + " = " + (soma + count));
+            soma = soma + count;
+            count = count + 1;
         }
+        System.out.println(soma);
     }
+
 }
