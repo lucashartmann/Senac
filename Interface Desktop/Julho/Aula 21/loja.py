1f9ba35a9f0ae8720803af2ec2e38093b2fac734
lucashartmann <lucas.a111@hotmail.com>
2025-07-23 09:37:47 -0300

    Aula 22.07

--- Interface Desktop/Julho/Aula 21/loja.py
+++ Interface Desktop/Julho/Aula 21/loja.py
@@ -4,23 +4,22 @@ from Item import Item
 from textual.events import Load
 from textual import on
 
+
 class AppZero(App):
 
     TITLE = "Loja"
-    
-  
+
     @on(Load)
     def inicializaçao(self):
         self.lista_items = list()
         for i in range(12):
             self.lista_items.append(Item())
-            
+
     def on_mount(self):
         list_view = self.query_one("#lst_item", ListView)
         for item in self.lista_items:
             list_view.append(ListItem(Label(item.nome)))
-        
-        
+
     def compose(self) -> ComposeResult:
         yield Header()
         yield ListView(id="lst_item")
@@ -32,6 +31,7 @@ class AppZero(App):
         lista = self.query_one("#lst_item", ListView)
         info = self.query_one("#tx_info", Label)
         info.update(self.lista_items[lista.index].nome)
+
 
 if __name__ == "__main__":
     app = AppZero()
