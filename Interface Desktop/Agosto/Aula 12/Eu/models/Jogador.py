c1f8cacbf1142a664ad2f7bf32bd87408ecc2ea0
lucashartmann <lucas.a111@hotmail.com>
2025-08-12 10:31:41 -0300

    Aula 12

---
+++ Interface Desktop/Agosto/Aula 12/Eu/models/Jogador.py
@@ -0,0 +1,10 @@
+class Jogador():
+    def __init__(self):
+        self.inventario = dict()
+        self.item_equipado = None
+        self.vida = 0
+        self.xp = 0
+        self.classe = ""
+        self.mana = 0
+        self.stamina = 0
+        self.nome = "sem nome"
