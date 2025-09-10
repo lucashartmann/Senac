9b0bf398c981968e82876d1ceacd9bbdc35d1e6f
lucashartmann <lucas.a111@hotmail.com>
2025-09-10 11:58:38 -0300

    Aula 10

--- Integração com Banco de Dados/Agosto/Aula 10/App.py
+++ Integração com Banco de Dados/Agosto/Aula 10/App.py
@@ -1,22 +1,25 @@
 import sys
 from InventarioDB import Banco
 
-comando = sys.argv[1:] # python .\App.py inserir lucas 10 
+comando = sys.argv[1:]
 
-um_banco =  Banco()
+um_banco = Banco()
+
 
 def menu(comando):
     match comando:
-        case ["inserir", nome_item, dano]:
+        case ["inserir", nome_item, dano]:  # python .\App.py inserir lucas 10
             print(um_banco.inserir((nome_item, dano)))
-        case ["remover", nome_item]:
+        case ["remover", nome_item]:  # python .\App.py remover lucas
             print(um_banco.deletar_por_nome(nome_item))
-        case ["atualizar", nome_item]:
-            print(um_banco.atualizar_item(nome_item))
-        case ["consultar", nome_item]:
-            print(um_banco.consulta_por_nome(nome_item))
-        case ["listar", nome_item]:
+        case ["atualizar", nome_item, novo_dano]:  # python .\App.py atualizar lucas 20
+            print(um_banco.atualizar_item(nome_item, novo_dano))
+        case ["consultar", nome_item]:  # python .\App.py consultar lucas
+            print(um_banco.consultar_por_nome(nome_item))
+        case ["listar"]:  # python .\App.py listar
             print(um_banco.consultar_itens())
+        case _:
+            print("Valor inválido!")
 
 
 if __name__ == "__main__":
