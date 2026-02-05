b51893a155c5c3aeb7628cdab04a2481035dc7c8
Lucas Hartmann <lucas.a111@hotmail.com>
2026-02-05 12:02:42 -0300

    Aula 05.02

---
+++ Desenvolvimento Web/Fevereiro 2026/Aula 05/script2.js
@@ -0,0 +1,40 @@
+let nome = "Roberto";
+console.log(nome);
+nome = "Lucas";
+console.log(nome);
+
+const IDADE = 31;
+console.log(IDADE);
+// IDADE = 32;
+// console.log(IDADE);
+
+console.log(typeof nome);
+console.log(typeof IDADE);
+
+let a = 10, b = 20, c = 30;
+
+console.log(a, b, c);
+
+const nomeCompleto = "Lucas";
+const nomecompleto = "Pedro Fernando";
+
+console.log(nomeCompleto, nomecompleto);
+
+let _teste = "Válido";
+
+console.log(_teste);
+
+let $teste = "Válido";
+console.log($teste);
+
+// const age = prompt("Digite sua idade");
+// console.log(`Você tem ${age} anos`);
+
+// alert("Testando");
+
+// const z = 10;
+// alert(`O número é ${z}`)
+
+console.log(Math.max(5, 2, 1, 20))
+console.log(Math.floor(5.14))
+console.log(Math.ceil(5.14))
