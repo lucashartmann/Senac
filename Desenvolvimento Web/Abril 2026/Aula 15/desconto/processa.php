5bc879ac43b88bc168a6e1f7c6d2273f535a9c00
lucashartmann <lucas.a111@hotmail.com>
2026-04-15 12:10:44 -0300

    +-

---
+++ Desenvolvimento Web/Abril 2026/Aula 15/desconto/processa.php
@@ -0,0 +1,8 @@
+<?php
+    if (__METHOD__ == ["$POST"]) {
+
+    } else {
+        echo "<p>Acesso Inválido!! Por favor, preencha o formulário</p>";
+        echo "<a href='index.html'>Voltar</a>";
+    }
+?>
