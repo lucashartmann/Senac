6755d4c6ac6d7509dab35312bde88610a4a4c21f
lucashartmann <lucas.a111@hotmail.com>
2026-05-20 12:06:35 -0300

    Aula 20

---
+++ Desenvolvimento Web/Maio 2026/Aula 20/php/view/buscarTalentosAjax.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include_once "../model/seguranca.php";
+include_once "../model/talentoDAO.php";
+
+Segurança::verificarAcesso();
+
+$filtro = trim($_GET["pesquisa"]);
+$talentoDAO = new TalentoDAO();
+
+$talentos = [];
+
+if (!empty($filtro)) {
+    $talentos = $talentoDAO->buscarPorFiltro($filtro);
+} else {
+    $talentos = $talentoDAO->listarTodos();
+}
+
+if (empty($talentos)) {
+    echo "<tr><td colspan='4'>Nenhum registro encontrado!</td></tr>";
+} else {
+
+    foreach ($talentos as $talento) {
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($talento['nome']) . "</td>";
+        echo "<td>" . htmlspecialchars($talento['email']) . "</td>";
+        echo "<td>" . htmlspecialchars($talento['fone']) . "</td>";
+        echo "<td> <span style='background-color: #21262d; padding: 4px 8px; border-radius: 4px; font-size: 0.85rem; border: 1px solid #30363d;'>" 
+        . htmlspecialchars($talento['tecnologia']) 
+        . "</span></td>";
+        echo "</tr>";
+    }
+}
